1f7d73af72647e046a105ca4fbebc43984d54494
Ashfaqbs <ash001x@gmail.com>
2024-08-11 09:14:36 +0530

    Logging username in api by request calls

--- Spring-Boot-security-V3J21/src/main/java/com/ashfaq/example/controller/SampleController.java
+++ Spring-Boot-security-V3J21/src/main/java/com/ashfaq/example/controller/SampleController.java
@@ -1,28 +1,60 @@
 package com.ashfaq.example.controller;
 
+import java.util.Base64;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleController {
 
+//Getting the username from the api calls
+	@GetMapping("/home")
+	public String handleWelcome() {
+		return "home";
+	}
 
-	  @GetMapping("/home")
-	  public String handleWelcome() {
-	    return "home";
-	  }
+	@GetMapping("/admin/home")
+	public String handleAdminHome() {
+		return "home_admin";
+	}
 
-	  @GetMapping("/admin/home")
-	  public String handleAdminHome() {
-	    return "home_admin";
-	  }
+	@GetMapping("/user/home")
+	public String handleUserHome(@RequestHeader("Authorization") String authHeader) {
 
-	  @GetMapping("/user/home")
-	  public String handleUserHome() {
-	    return "home_user";
-	  }
+//		// Get the authenticated user from @RequestHeader
+		String username = extractUsernameFromAuthHeader(authHeader);
+		System.out.println("Username from Header: " + username);
 
-	
+//        OR 
 
+		// Get the authenticated user from SecurityContextHolder
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String usernamefromContext = authentication.getName();
+
+		// Log the username
+		System.out.println("User '{}' is creating a car" + usernamefromContext);
+
+		// logs
+//        Username from Header: seller
+//     User  is creating a car 'seller'
+
+		return "home_user";
+	}
+
+	//
+	private String extractUsernameFromAuthHeader(String authHeader) {
+		if (authHeader != null && authHeader.startsWith("Basic ")) {
+			// Decode the Base64 encoded username:password
+			String base64Credentials = authHeader.substring(6);
+			String credentials = new String(Base64.getDecoder().decode(base64Credentials));
+			// Split the credentials to get the username
+			return credentials.split(":")[0];
+		}
+		return null;
+	}
 
 }
